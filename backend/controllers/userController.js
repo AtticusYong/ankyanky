a3b1103253b919d59470542d3b04ad884c95c173
susukacang <yongsoonfatt@gmail.com>
2023-05-12 15:11:09 -0700

    67. Admin Middleware & Get Users Endpoint
    Info: using POSTMAN to login as admin and then get users. access allowed since is admin

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -117,4 +117,13 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile };
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  // forgotten await and empty object {} was returned
+  const users = await User.find({});
+  res.json(users)
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers };
