aa7f061385d2e0a9abc40eea2ede8804c38cf75c
susukacang <yongsoonfatt@gmail.com>
2023-05-18 16:20:55 -0700

    Fix Lect. 83
    Issues:
    1. Unable to updateOrderToPaid
    insert back route for "/:id/pay" in OrderRoutes.js. it was replaced with route for "/:id/deliver"
    2.

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -16,6 +16,10 @@ import {
   ORDER_LIST_REQUEST,
   ORDER_LIST_SUCCESS,
   ORDER_LIST_FAIL,
+  ORDER_DELIVER_REQUEST,
+  ORDER_DELIVER_SUCCESS,
+  ORDER_DELIVER_FAIL,
+  ORDER_DELIVER_RESET,
   // ORDER_LIST_RESET,
 } from "../constants/orderConstants";
 
@@ -70,7 +74,6 @@ export const orderPayReducer = (state = {}, action) => {
   switch (action.type) {
     case ORDER_PAY_REQUEST:
       return {
-        ...state,
         loading: true,
       };
     case ORDER_PAY_SUCCESS:
@@ -84,6 +87,30 @@ export const orderPayReducer = (state = {}, action) => {
         error: action.payload,
       };
     case ORDER_PAY_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const orderDeliverReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ORDER_DELIVER_REQUEST:
+      return {
+        // ...state,
+        loading: true,
+      };
+    case ORDER_DELIVER_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+      };
+    case ORDER_DELIVER_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case ORDER_DELIVER_RESET:
       return {};
     default:
       return state;
